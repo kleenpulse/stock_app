af7fd20cc56dffee70af0a788841d30669d567e8
Vxrcel <skyeparagon@gmail.com>
2025-09-30 17:42:39 +0100

    Add MongoDB and Mongoose integration with connection handling

---
+++ database/mongoose.ts
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const MONGODB_URL = process.env.MONGODB_URI;
+
+declare global {
+	var mongooseCache: {
+		conn: typeof mongoose | null;
+		promise: Promise<typeof mongoose> | null;
+	};
+}
+
+let cached = global.mongooseCache;
+if (!cached) {
+	cached = global.mongooseCache = { conn: null, promise: null };
+}
+
+export const connectToDatabase = async () => {
+	if (!MONGODB_URL) {
+		throw new Error(
+			"Please define the MONGODB_URI environment variable inside .env.local"
+		);
+	}
+
+	if (cached.conn) {
+		return cached.conn;
+	}
+	if (!cached.promise) {
+		cached.promise = mongoose.connect(MONGODB_URL, { bufferCommands: false });
+	}
+
+	try {
+		cached.conn = await cached.promise;
+	} catch (error) {
+		cached.promise = null;
+		throw error;
+	}
+
+	console.log(`Connected to DB ${process.env.NODE_ENV} - ${MONGODB_URL}`);
+};
